5808d011ec50b05604b81c90656126e5799d01db
claverlo <leopac84@icloud.com>
2026-01-24 06:13:06 -0800

    106 assignment #2

---
+++ scripts/Task.js
@@ -0,0 +1,13 @@
+class Task {
+    constructor(title, description, color, date, status, budget) {  //A parameter is a placeholder name for data that a function or method expects.
+        this.title = title;//Parameters = names (placeholders) title, description, color, date, status, budget
+        this.desc = description;// Values come from the user's input in the HTML form
+        this.color = color; //desc= Note: mapped to desc internally
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+    }
+}
+
+// Note: To format this file (auto-fix spacing/indentation so the code looks neat and consistent) in VS Code,
+// right-click and select "Format Document".
